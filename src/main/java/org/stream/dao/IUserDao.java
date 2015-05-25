00c8bb478b5bc1acb5e8c724d04c024b196234ac
krisjin <krisibm@163.com>
2015-05-25 15:51:06 +0800

    config

---
+++ src/main/java/org/stream/dao/IUserDao.java
@@ -0,0 +1,15 @@
+package org.stream.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/5/25
+ * Time: 13:56
+ */
+@Repository
+public interface IUserDao {
+
+
+}
---
+++ src/main/java/org/stream/service/IUserService.java
@@ -0,0 +1,10 @@
+package org.stream.service;
+
+/**
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/5/25
+ * Time: 14:54
+ */
+public interface IUserService {
+}

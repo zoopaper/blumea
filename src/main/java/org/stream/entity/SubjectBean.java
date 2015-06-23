ac02ac297480ad84c603d989eedabd1527a19691
krisjin <krisibm@163.com>
2015-06-24 00:08:40 +0800

    subject

--- src/main/java/org/stream/entity/SubjectBean.java
+++ src/main/java/org/stream/entity/SubjectBean.java
@@ -2,6 +2,8 @@ package org.stream.entity;
 
 import org.stream.entity.base.IMybatisEntity;
 
+import java.sql.Timestamp;
+
 /**
  * 栏目
  * <p/>
@@ -11,5 +13,104 @@ import org.stream.entity.base.IMybatisEntity;
  */
 public class SubjectBean extends IMybatisEntity {
 
+    private long pid;
 
+    private String category;//父对象的全路径，以";"分割
+
+    private String shortName;//发布英文名称
+
+    private String name;    //栏目名字
+
+    private long channelId;
+
+
+    private long templateId;
+
+    private int status;
+
+    private int priority;
+
+    private int type;
+
+    private String tags;    //关键词
+
+    private String desc;    //描述
+
+    private Timestamp createTime;
+
+    private Timestamp updateTime;
+
+
+    public long getPid() {
+        return pid;
+    }
+
+    public void setPid(long pid) {
+        this.pid = pid;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(long channelId) {
+        this.channelId = channelId;
+    }
+
+
+    public long getTemplateId() {
+        return templateId;
+    }
+
+    public void setTemplateId(long templateId) {
+        this.templateId = templateId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
 }
